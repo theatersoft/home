953a160ae9ef581a8138cc6bbe18e0e487406b25
Rob Huehn <rhuehn@gmail.com>
2017-03-13 16:13:16 -0700

    config and deploy

--- deploy.js
+++ deploy.js
@@ -1,61 +1,67 @@
 'use strict'
 require('shelljs/make')
-
 const
-//path = require('path'),
+    DEST = '/opt/theatersoft',
     fs = require('fs'),
     pkg = require('./package.json'),
     cfg = require('./config.json'),
-    DEST = '/opt/theatersoft',
-    execo = c => exec(c, {silent: true}).stdout.trim()
+    {hosts = []} = cfg,
+    hostname = require('os').hostname(),
+    execo = c => exec(c, {silent: true}).stdout.trim(),
+    write = (file, json) => fs.writeFileSync(file, JSON.stringify(json, null, '  '), 'utf-8'),
+    log = console.log
 
 const targets = {
-    opt () {
-        exec(`mkdir -p ${DEST}`)
-        exec(`cp client/theatersoft-*.tgz server/theatersoft-*.tgz ${DEST}`)
-    },
-
-    pack () {
-        const {hosts = []} = cfg
+    config () {
+        log('target config')
         exec(`mkdir -p deploy`)
+        const
+            find = module => execo(`cd server; npm explore ${module} pwd`),
+            pack = path => execo(`cd deploy; npm pack ${path}`),
+            baseDependencies = {
+                "@theatersoft/client": pack(find('@theatersoft/client')),
+                "@theatersoft/server": pack(find('@theatersoft/server')),
+                "@theatersoft/bus": pack(find('@theatersoft/bus'))
+            }
+        log('Base dependencies:\n')
+        log(baseDependencies)
+        log('\nService dependencies:')
         hosts.forEach(({name, services = []}) => {
-            console.log(`\n${name}`)
-            services.forEach(({module}) => {
-                const path = execo(`cd server; npm explore ${module} pwd`)
-                console.log('└──', module)
-                console.log('    ', path)
-                const file = execo(`cd ${path}; npm pack`)
-                exec(`cd deploy; mv ${path}/${file} .`)
-            })
+            log(`\n${name}`)
+            targets.package(name, services.reduce((o, {module}) => {
+                const path = find(module)
+                log('└──', module)
+                o[module] = pack(path)
+                log('    ', path, '\n    ...', o[module])
+                return o
+            }, Object.assign({}, baseDependencies)))
         })
+        Object.assign(pkg.scripts, hosts.reduce((o, {name}) =>
+            ((o[`deploy-${name}`] = `node deploy deploy -- ${name}`), o), {}))
+        write('package.json', pkg)
     },
 
-    package () {
-        const p = Object.assign({}, pkg, {
-            dependencies: {
-                "@theatersoft/client": "~1",
-                "@theatersoft/server": "~3"
-            },
+    package (host, dependencies) {
+        exec(`mkdir -p deploy/${host}`)
+        write(`deploy/${host}/package.json`, Object.assign({}, pkg, {
+            dependencies,
             devDependencies: undefined,
-            scripts: undefined
-        })
-        fs.writeFileSync(
-            `${DEST}/package.json`
-            , JSON.stringify(p, null, '  '), 'utf-8')
+            scripts: pkg.deployScripts,
+            deployScripts: undefined
+        }))
     },
 
-    npmi () {
-        exec(
-            `cd ${DEST}; for i in *.tgz; do npm install $i; done`
-        )
-    },
-
-    all () {
-        targets.opt()
-        targets.pack()
-        //targets.package()
-        //targets.npmi()
+    deploy (host) {
+        if (!host) return hosts.forEach(({name}) => targets.deploy(name))
+        if (Array.isArray(host)) host = host[0]
+        log('target host', host)
+        if (host === hostname) {
+            exec(`mkdir -p ${DEST}`)
+            exec(`cp deploy/*.tgz deploy/${host}/package.json ${DEST}`)
+            exec(`cd ${DEST}; npm install`)
+        } else {
+            log('TODO')
+        }
     }
 }
-
 Object.assign(target, targets)
